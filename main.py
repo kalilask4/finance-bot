af706f988180dc35449db0442ca6e60e1fdcbdb8
inity8 <di848588@gmail.com>
2020-06-20 23:53:44 +0300

    init, add an example

---
+++ main.py
@@ -0,0 +1,41 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = 'BOT TOKEN HERE'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+
+@dp.message_handler(regexp='(^cat[s]?$|puss)')
+async def cats(message: types.Message):
+    with open('data/cats.jpg', 'rb') as photo:
+        '''
+        # Old fashioned way:
+        await bot.send_photo(
+            message.chat.id,
+            photo,
+            caption='Cats are here 😺',
+            reply_to_message_id=message.message_id,
+        )
+        '''
+
+        await message.reply_photo(photo, caption='Cats are here 😺')
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
